fe6f189d4a3f40de4ff6a62977aa686cba2af13f
diepgiahan <dghan98@gmail.com>
2019-06-02 22:41:28 +0700

    .

--- A02/Assets/InstantiationExample.cs
+++ A02/Assets/InstantiationExample.cs
@@ -10,15 +10,31 @@ public class InstantiationExample : MonoBehaviour
     // This script will simply instantiate the Prefab when the game starts.
     void Start()
     {
-        // Instantiate at position (0, 0, 0) and zero rotation.
-        //Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
-        for (int i = 0; i < 10; i++)
-        {
-            Instantiate(prefab, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
-        }
+    // Instantiate at position (0, 0, 0) and zero rotation.
+    //Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+        for (int i = 0; i < 5; i++)
+            for (int j = 0; j < 2; j++)
+            {
+                Instantiate(prefab, new Vector3(i * 0.5f, j*0.5f, 10), Quaternion.identity);
+            }
     }
-    void Update()
-    {
+    //void Update()
+    //{
 
-    }
+    //}
+    /*public int numberOfObjects = 20;
+    public float radius = 3f;
+    void Start()
+    {
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            float angle = i * Mathf.PI * 2 / numberOfObjects;
+            float x = Mathf.Cos(angle) * radius;
+            float y = Mathf.Sin(angle) * radius;
+            Vector3 pos = transform.position + new Vector3(x, y, 10);
+            float angleDegrees = -angle * Mathf.Rad2Deg;
+            Quaternion rot = Quaternion.Euler(0, angleDegrees, 0);
+            Instantiate(prefab, pos, rot);
+        }
+    }*/
 }
