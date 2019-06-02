b88fb5ff48fa19363b059beab038f13030b22084
diepgiahan <dghan98@gmail.com>
2019-06-02 22:18:08 +0700

    .

---
+++ A02/Assets/InstantiationExample.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiationExample : MonoBehaviour
+{
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject prefab;
+
+    // This script will simply instantiate the Prefab when the game starts.
+    void Start()
+    {
+        // Instantiate at position (0, 0, 0) and zero rotation.
+        //Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(prefab, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+        }
+    }
+    void Update()
+    {
+
+    }
+}
