fcabc147df206b0c58585ff9a70d872b2768e31b
Nguyen Minh Nhut <nmnhut@apcs.vn>
2019-07-05 13:29:54 +0700

    right project tile

---
+++ MidtermProj/Assets/CharacterMovement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float HP = 10.0f;
+    public bool isSliding = false;
+    public bool isJumping = false;
+    public float llllll = 0.0f;
+    public Rigidbody2D rigidBody2D;
+    public float thrustForce = 100000.0f;
+    public Animator animator;
+
+    void Start()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)) 
+            animator.SetFloat("characterSpeed", 2.0f);
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetFloat("characterSpeed", 2.0f);
+        }
+        else animator.SetFloat("characterSpeed", 0.0f); 
+       
+
+    }
+    // Update is called once per frame
+    private void Awake()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetFloat("characterSpeed", 2.0f);
+           
+        }
+        else animator.SetFloat("characterSpeed", 0.0f);
+    }
+
+    void Update()
+    {
+        //float mmmmm = 0;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //animator.SetFloat("characterSpeed", 2.0f);
+            rigidBody2D.transform.Translate(Vector2.left*thrustForce);
+            animator.SetFloat("characterSpeed", 2.0f);
+
+            //animator.SetFloat("Speed", 0.2f); 
+            //transform.position.Set(transform.position.x + thrustForce, transform.position.y, transform.position.z);
+
+            //mmmmm = 2.0f;
+        }
+        else
+            if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidBody2D.transform.Translate(Vector2.right*thrustForce);
+            animator.SetFloat("characterSpeed", 2.0f);
+
+            //animator.SetFloat("Speed", 0.2f); 
+            //transform.position.Set(transform.position.x + thrustForce, transform.position.y, transform.position.z);
+
+            //mmmmm = 2.0f;
+        }
+
+        //else llllll = 0;
+
+        //animator.SetFloat("characterSpeed", mmmmm);
+        //animator.SetBool("isSliding", isSliding);
+        //animator.SetBool("isJumping", isJumping);
+        //animator.SetFloat("HP", HP);
+    }
+}
---
+++ MidtermProj/Assets/icePath.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class icePath : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public Rigidbody2D rigixdbody; 
+    public bool isFlying = false;
+    public float throwAngle = 60; 
+    public float throwForce;
+    private float flyingTime = 0;
+    public float gravity = 10f;
+    private float preY,preX; 
+    private float x0, y0;  
+    void Start()
+    {
+        //GetComponent<Rigidbody2D>().centerOfMass = new Vector2(0.1f, 0); 
+    }
+    private void Awake()
+    {
+        isFlying = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = Mathf.PI * throwAngle / 180;
+        if (Input.GetKey(KeyCode.Space) && !isFlying)
+        {
+            float forceY = Mathf.Sin(angle) * throwForce*2 ;
+            float forceX = Mathf.Cos(angle) * throwForce/2;
+
+            //float oldX = transform.rotation.x;
+            //float oldY = transform.rotation.y;
+            //float oldW = transform.rotation.w;
+            //preX = oldX;
+            //preY = oldY; 
+            //transform.rotation.Set(oldX, oldY, angle, oldW); 
+            //GetComponent<Rigidbody2D>().AddForce(new Vector2(forceX, forceY));
+            transform.eulerAngles = new Vector3(0, 0, throwAngle); 
+            x0 = transform.position.x;
+            y0 = transform.position.y;
+            flyingTime = 0;
+            //flyingTime += Time.deltaTime; 
+            isFlying = true;
+            //GetComponent<Rigidbody2D>().
+            preX = preY = 0; 
+
+        }
+
+        if (isFlying)
+        { 
+        float x = x0 + throwForce * flyingTime * Mathf.Cos(angle);
+            float y = y0 + throwForce * flyingTime * Mathf.Sin(angle) - 0.5f * gravity * flyingTime * flyingTime;
+            transform.position = new Vector3(x, y, 0.0f);
+
+            //Debug.Log("postion: "+  x + " " + y + "flyingTime:" + flyingTime ); 
+            float _tan = Mathf.Tan(angle);
+            var _cos = Mathf.Cos(angle);
+            var _sin = Mathf.Sin(angle);
+
+            float dy = _tan - (x-x0) * gravity/(throwForce*throwForce*_cos*_cos) ;
+
+            if (preY > y)
+            {
+                //dy =  (x - x0) * (1+ _tan * _tan) * gravity / (throwForce * throwForce);
+
+            }
+            float lineAngle= System.Math.Abs(dy) < 0.01 ? 0 : Mathf.Atan(dy)*180f/Mathf.PI;
+            //lineAngle = Mathf.Abs(lineAngle);
+
+            preY = y; 
+            transform.eulerAngles = new Vector3(0, 0,lineAngle);
+            //Debug.Log("Line Angle: "+ lineAngle); 
+            flyingTime += Time.deltaTime/10;
+
+            if (y < y0)
+            {
+                isFlying = false;
+                flyingTime = 0; 
+            }
+        }
+
+
+
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isFlying = false; 
+    }
+}
